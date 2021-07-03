5d2d53a29aa850894eddd4f2a35b33e6e6116440
Pinkie_Pie <huangkaiqiao@gmail.com>
2021-07-03 11:30:24 +0000

    sha256

--- src/main.h
+++ src/main.h
@@ -1,46 +1,20 @@
 #include <iostream>
 #include <string>
-
+#include <stdarg.h>
+#include <stdint.h>
 #include "spdlog/spdlog.h"
 
-using std::string; using std::hex;
-std::stringstream ss;
+using std::hex;
+using std::string;
+using std::stringstream;
+using std::vector;
+std::stringstream SS;
+
+typedef unsigned int uint32;
+typedef unsigned char u8;
+typedef unsigned u32;
 
 int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
-
-// std::string SHA256(std::string message) {
-//     //初始哈希值
-// 	unsigned int h0 = 0x6a09e667,
-// 	    h1 = 0xbb67ae85,
-// 	    h2 = 0x3c6ef372,
-// 	    h3 = 0xa54ff53a,
-// 	    h4 = 0x510e527f,
-// 	    h5 = 0x9b05688c,
-// 	    h6 = 0x1f83d9ab,
-// 	    h7 = 0x5be0cd19;
-//     unsigned int k[64] = {
-//         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-// 		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-// 		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-// 		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-// 		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-// 		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-// 		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-// 		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-//     };
-//     string padded = message.append('\x80');
-//     if(padded.size() % 64 < 56) {
-//         int suffixLen = 64 + 56 - (padded.size() % 64);
-//         padded.append('\x00', suffixLen);
-//     } else {
-//         int suffixLen = 56 - (padded.size() % 64);
-//         padded.append('\x00', suffixLen);
-//     }
-//     unsigned long msgLen = message.size();
-//     src.append(std::to_string(msgLen));
-    
-//     char broken[][] = 
-// }
 
 // hex array to string
 // std::string HexToString(const unsigned char* data,int len)
@@ -76,22 +50,233 @@ std::string StringToHex(string s)
     return str;
 }
 
+string Uint64ToString(uint64_t num) {
+    // unsigned char arr[8];
+    stringstream ss;
+    // ss.str("");
+    ss << (unsigned char)((num >> 56) & 0xFF); 
+    ss << (unsigned char)((num >> 48) & 0xFF); 
+    ss << (unsigned char)((num >> 40) & 0xFF);
+    ss << (unsigned char)((num >> 32) & 0xFF); 
+    ss << (unsigned char)((num >> 24) & 0xFF); // 0x12 
+    ss << (unsigned char)((num >> 16) & 0xFF); // 0x34
+    ss << (unsigned char)((num >> 8) & 0xFF); // 0x56
+    ss << (unsigned char)(num & 0xFF); // 0x78
+    return ss.str();
+}
+
+string U32ToString(u32 num) {
+    stringstream ss;
+    ss << u8((num >> 24) & 0xFF); // 0x12 
+    ss << u8((num >> 16) & 0xFF); // 0x34
+    ss << u8((num >> 8) & 0xFF);  // 0x56
+    ss << u8(num & 0xFF);         // 0x78
+    return ss.str();
+}
+
+void SplitString(const std::string& s, std::vector<std::string>& v, const std::string& c)
+{
+  std::string::size_type pos1, pos2;
+  pos2 = s.find(c);
+  pos1 = 0;
+  while(std::string::npos != pos2)
+  {
+    v.push_back(s.substr(pos1, pos2-pos1));
+ 
+    pos1 = pos2 + c.size();
+    pos2 = s.find(c, pos1);
+  }
+  if(pos1 != s.length())
+    v.push_back(s.substr(pos1));
+}
+
 // Pre-processing (Padding): 预处理
-std::string preProcessing(std::string message) {
-    unsigned long msgLen = message.size();
+std::string PreProcessing(std::string message) {
+    unsigned long long msgLen = message.size();
     // spdlog::info("preProcessing: msgLen={}", msgLen);
     std::string padded = message + '\x80';
-    int len = padded.size();
+    int len = padded.size() + 8;
     int tmp = len % 64;
     tmp = tmp == 0? 64:64-tmp;
-    spdlog::info("preProcessing: message={} tmp={}", message, tmp);
+    // spdlog::info("PreProcessing: message={} tmp={}", message, tmp);
     padded.append(tmp, '\x00');
-    unsigned char arr[2];
-    arr[0] = (msgLen >> 8) & 0xFF; // 0x56
-    arr[1] = msgLen & 0xFF; // 0x78
-    ss.str("");
-    ss << padded;
+    SS.str("");
+    SS << padded;
     // ss << (unsigned char*)&msgLen;
-    ss << arr;
-    return ss.str();
+    SS << Uint64ToString(msgLen);
+    return SS.str();
+}
+
+vector<string> DivideString(string s, int part_size) {
+     
+    int str_size = s.size();
+    int i;
+    vector<string> res;
+ 
+    // Check if string can be divided in
+    // n equal parts
+    if (str_size % part_size != 0)
+    {
+        spdlog::error("Invalid Input: String size");
+        spdlog::error(" is not divisible by n");
+        throw "Unexception message length " + std::to_string(str_size);
+    }
+    int n = str_size / part_size;
+ 
+    // Calculate the size of parts to
+    // find the division points
+    for (i=0; i<n; i++) {
+        res.push_back(s.substr(i*part_size, part_size));
+    }
+    // for(i=0; i<res.size(); i++){
+    //     spdlog::info("vec[{}]={}", i, res[i]);
+    // }
+    return res;
+}
+
+// (The initial values in w[0..63] don't matter, so many implementations zero them here)
+void copyByte64ToU32Arr(string chunk, u32* pW) {
+    const unsigned char* data = (unsigned char *)chunk.data();
+    for(int i=0; i<16; i++){
+        // *pW = 0;
+        // memcpy(pW+i, data+i*8, 8); 
+        *(pW+i) = u32(
+            (u8)*(data+i*4) << 24 |
+            (u8)*(data+i*4+1) << 16 |
+            (u8)*(data+i*4+2) << 8 |
+            (u8)*(data+i*4+3)
+        );
+    }
+    spdlog::info("copy: chunk={}", StringToHex(chunk));
+    for(int i=0; i<16; i++){
+        spdlog::info("copy: w[{}]={}", i, *(pW+i));
+    }
+}
+
+/*循环左移*/
+unsigned rol(unsigned val, int size)
+{
+  unsigned res = val << size;
+  res |= val >> (32 - size);
+  return res;
+}
+
+/*循环右移*/
+unsigned ror(unsigned val, int size)
+{
+  unsigned res = val >> size;
+  res |= val << (32 - size);
+  return res;
+}
+
+u32 sum_mod32(u32 cnt, ...) {
+    u32 sum = 0;
+    va_list ap;
+    va_start(ap, cnt);
+    for (int i = 0; i < cnt; ++i) {
+        sum += va_arg(ap, u32);
+        sum = sum & 0xffffffff;
+    }
+    va_end(ap);
+    return sum;
+}
+
+string SHA256(string message) {
+    //初始哈希值
+	u32 h0 = 0x6a09e667,
+	    h1 = 0xbb67ae85,
+	    h2 = 0x3c6ef372,
+	    h3 = 0xa54ff53a,
+	    h4 = 0x510e527f,
+	    h5 = 0x9b05688c,
+	    h6 = 0x1f83d9ab,
+	    h7 = 0x5be0cd19;
+
+    u32 k[64] = {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    };
+
+    string padded = PreProcessing(message);
+    vector<string> chunks = DivideString(padded, 64);
+    vector<string>::iterator it = chunks.begin();
+    while(it != chunks.end()) {
+        uint32 w[64];
+        string chunk = *it;
+        copyByte64ToU32Arr(chunk, w);
+        spdlog::info("copyByte64ToU32Arr: {}, {:x}", w[0], w[0]);
+        // Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array:
+        for (int i=16; i<64; i++) {
+            uint32 s0 = ror(w[i-15],  7) ^ ror(w[i-15], 18) ^ (w[i-15] >> 3);
+            uint32 s1 = ror(w[i- 2], 17) ^ ror(w[i- 2], 19) ^ (w[i-2] >> 10);
+            w[i] = w[i-16] + s0 + w[i-7] + s1;
+            // w[i] = sum_mod32(4, w[i-16], s0, w[i-7], s1);
+        }
+
+        // Initialize working variables to current hash value:
+        u32 a = h0,
+        b = h1,
+        c = h2,
+        d = h3,
+        e = h4,
+        f = h5,
+        g = h6,
+        h = h7;
+
+        // Compression function main loop:
+        for(int i=0; i<64; i++){
+            spdlog::info("SHA256: a={:x}", a);
+            u32 S1 = ror(e, 6) ^ ror(e, 11) ^ ror(e, 25);
+            u32 ch = (e & f) ^ ((~ e) & g);
+            // u32 temp1 = h + S1 + ch + k[i] + w[i];
+            spdlog::info("t1: {:x} {:x} {:x} w[{}]:{:x} {:x}", S1, ch, h, i, w[i], k[i]);
+            u32 temp1 = sum_mod32(5, h, S1, ch, k[i], w[i]);
+            u32 S0 = ror(a, 2) ^ ror(a, 13) ^ ror(a, 22);
+            u32 maj = (a & b) ^ (a & c) ^ (b & c);
+            // u32 temp2 = S0 + maj;
+            u32 temp2 = sum_mod32(2, S0, maj);
+    
+            h = g;
+            g = f;
+            f = e;
+            // e = d + temp1;
+            e = sum_mod32(2, d, temp1);
+            d = c;
+            c = b;
+            b = a;
+            // a = temp1 + temp2;
+            a = sum_mod32(2, temp1, temp2);
+            spdlog::info("SHA256: temp1={:x}, temp2={:x}", temp1, temp2);
+        }
+
+        // Add the compressed chunk to the current hash value:
+        h0 = sum_mod32(2, h0, a);
+        h1 = sum_mod32(2, h1, b);
+        h2 = sum_mod32(2, h2, c);
+        h3 = sum_mod32(2, h3, d);
+        h4 = sum_mod32(2, h4, e);
+        h5 = sum_mod32(2, h5, f);
+        h6 = sum_mod32(2, h6, g);
+        h7 = sum_mod32(2, h7, h);
+
+        it++;
+    }
+
+    // Produce the final hash value (big-endian):
+    string hash = "";
+    hash.append(U32ToString(h0))
+        .append(U32ToString(h1))
+        .append(U32ToString(h2))
+        .append(U32ToString(h3))
+        .append(U32ToString(h4))
+        .append(U32ToString(h5))
+        .append(U32ToString(h6))
+        .append(U32ToString(h7));
+    return hash;
 }
--- src/test.cc
+++ src/test.cc
@@ -9,14 +9,6 @@ TEST_CASE("testing the factorial function") {
     CHECK(factorial(10) == 3628800);
 }
 
-// TEST_CASE("testing the sha256 function") {
-//     CHECK(SHA256("") == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
-//     CHECK(SHA256(" ") == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
-//     CHECK(SHA256("123456") == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
-//     CHECK(SHA256("BlockChain") == "3a6fed5fc11392b3ee9f81caf017b48640d7458766a8eb0382899a605b41f2b9")
-//     CHECK(SHA256("从而排解心情，丰富自身精神面貌") == "ad33913bd36726df41f5a14cf7a1d1ef4c6961071b5b30aed1d4c436c2b77c08");
-// }
-
 TEST_CASE("testing string to hex") {
     CHECK(StringToHex(string{'\0'}) == "00");
     CHECK(StringToHex(" ") == "20");
@@ -25,9 +17,40 @@ TEST_CASE("testing string to hex") {
     CHECK(StringToHex("从而排解心情，丰富自身精神面貌") == "e4bb8ee8808ce68e92e8a7a3e5bf83e68385efbc8ce4b8b0e5af8ce887aae8baabe7b2bee7a59ee99da2e8b28c");
 }
 
+TEST_CASE("testing uint to string") {
+    CHECK(StringToHex(Uint64ToString(0ULL))                    == "0000000000000000");
+    CHECK(StringToHex(Uint64ToString(1024ULL))                 == "0000000000000400");
+    CHECK(StringToHex(Uint64ToString(2147483647ULL))           == "000000007fffffff");
+    CHECK(StringToHex(Uint64ToString(4294967295ULL))           == "00000000ffffffff");
+    CHECK(StringToHex(Uint64ToString(18446744073709551615ULL)) == "ffffffffffffffff");
+}
+
+TEST_CASE("testing devide string") {
+    vector<string> vec1 = DivideString("StreamingAudioService", 7);
+    vector<string> vec2 = {"Streami", "ngAudio", "Service"};
+    CHECK(std::equal(vec1.begin(), vec1.end(), vec2.begin()));
+}
+
 TEST_CASE("testing pre processing") {
-    CHECK(StringToHex(preProcessing(""))           == "8000000000000000000000000000000000000000000000000000000000000000");
-    CHECK(StringToHex(preProcessing(" "))          == "2010000000000000000000000000000000000000000000000000000000000001");
-    CHECK(StringToHex(preProcessing("123456"))     == "3132333435361000000000000000000000000000000000000000000000000006");
-    CHECK(StringToHex(preProcessing("BlockChain")) == "426c6f636b436861696e0000000000000000000000000000000000000000000a");
+    CHECK(StringToHex(PreProcessing(""))           == "80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    CHECK(StringToHex(PreProcessing(" "))          == "20800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001");
+    CHECK(StringToHex(PreProcessing("123456"))     == "31323334353680000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006");
+    CHECK(StringToHex(PreProcessing("BlockChain")) == "426c6f636b436861696e80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a");
+}
+
+TEST_CASE("testing copy char* to int") {
+    string chunk = "80000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    uint32 w[16];
+    copyByte64ToU32Arr(chunk, w);
+    CHECK(w[0] == 942682160);
+    CHECK(w[1] == 808464432);
+    CHECK(w[15] == 808464432);
+}
+
+TEST_CASE("testing the sha256 function") {
+    CHECK(StringToHex(SHA256("")) == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    // CHECK(StringToHex(SHA256(" ")) == "36a9e7f1c95b82ffb99743e0c5c4ce95d83c9a430aac59f84ef3cbfab6145068");
+    // CHECK(StringToHex(SHA256("123456")) == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
+    // CHECK(StringToHex(SHA256("BlockChain")) == "3a6fed5fc11392b3ee9f81caf017b48640d7458766a8eb0382899a605b41f2b9");
+    // CHECK(StringToHex(SHA256("从而排解心情，丰富自身精神面貌")) == "ad33913bd36726df41f5a14cf7a1d1ef4c6961071b5b30aed1d4c436c2b77c08");
 }
